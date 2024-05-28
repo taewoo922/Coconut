62faaea274e440d3072ebfecc63f7b049ca5c03c
kimminseop99 <161572638+kimminseop99@users.noreply.github.com>
2024-05-28 17:43:39 +0900

    기본 패키지 작성 (#4)

---
+++ src/main/java/com/example/coconut/global/jpa/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.example.coconut.global.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+
+
+
+}
