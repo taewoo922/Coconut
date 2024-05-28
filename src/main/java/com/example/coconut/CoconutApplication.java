4a7b77f53f23176a0bc446a4eeda086ae08194e1
taewoo922 <kimtaewoo922@gmail.com>
2024-05-28 16:36:50 +0900

    최초

---
+++ src/main/java/com/example/coconut/CoconutApplication.java
@@ -0,0 +1,13 @@
+package com.example.coconut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoconutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoconutApplication.class, args);
+	}
+
+}
