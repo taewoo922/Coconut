ea574b78f991b32040c8e29ef128a2ad88b4967f
taewoo922 <kimtaewoo922@gmail.com>
2024-05-29 18:42:03 +0900

    자유토론 #2

--- src/main/java/com/example/coconut/domain/discussion_Type/controller/FreedcsController.java
+++ src/main/java/com/example/coconut/domain/discussion_Type/controller/FreedcsController.java
@@ -3,10 +3,14 @@ package com.example.coconut.domain.discussion_Type.controller;
 import com.example.coconut.domain.discussion_Type.entity.Freedcs;
 import com.example.coconut.domain.discussion_Type.service.FreedcsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -16,6 +20,8 @@ import java.util.List;
 public class FreedcsController {
     private final FreedcsService freedcsService;
 
+
+
     @GetMapping("/freedcs_list")
     public String list(Model model) {
         List<Freedcs> freedcsList = this.freedcsService.getList();
@@ -24,5 +30,18 @@ public class FreedcsController {
         return "discussion/freedcs_list";
 
     }
+    @GetMapping("/free_create")
+    public String free_create() {
+        return "discussion/free_create_form";
+    }
+
+    @PostMapping("/free_create")
+    public String free_create(@RequestParam("title") String title, @RequestParam("content") String content, @RequestParam("thumbnail") MultipartFile thumbnail) {
+        freedcsService.free_create(title,content,thumbnail);
+
+        return "redirect:/discussion/freedcs_list";
+    }
+
+
 
 }
--- src/main/java/com/example/coconut/domain/discussion_Type/service/FreedcsService.java
+++ src/main/java/com/example/coconut/domain/discussion_Type/service/FreedcsService.java
@@ -3,26 +3,45 @@ package com.example.coconut.domain.discussion_Type.service;
 import com.example.coconut.domain.discussion_Type.entity.Freedcs;
 import com.example.coconut.domain.discussion_Type.repository.FreedcsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class FreedcsService {
     private final FreedcsRepository freedcsRepository;
 
+    @Value("${custom.fileDirPath}")
+    private String fileDirPath;
+
     public List<Freedcs> getList() {
         return freedcsRepository.findAll();
     }
 
-//    public void create(String title, String content) {
-//        Freedcs freedcs = Freedcs.builder()
-//                .title(title)
-//                .content(content)
-//                .build();
-//        freedcsRepository.save(freedcs);
-//    }
+    public void free_create(String title, String content,MultipartFile thumbnail) {
+        String thumbnailRelPath = "freedcs/" + UUID.randomUUID().toString() + ".jpg";
+        File thumbnailFile = new File(fileDirPath + "/" +thumbnailRelPath);
+
+        try {
+            thumbnail.transferTo(thumbnailFile);
+        } catch( IOException e ) {
+            throw new RuntimeException(e);
+        }
+
+        Freedcs freedcs = Freedcs.builder()
+                .title(title)
+                .content(content)
+                .thumbnailImg(thumbnailRelPath)
+                .build();
+        freedcsRepository.save(freedcs);
+    }
+
 
 }
