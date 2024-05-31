8fff49185245199acaaadae5531a0ce5b330abe2
kimminseop99 <minseopkim99@gmail.com>
2024-05-31 17:57:39 +0900

    ãreport form, detail, list 수정

--- src/main/java/com/example/coconut/domain/report/controller/ReportController.java
+++ src/main/java/com/example/coconut/domain/report/controller/ReportController.java
@@ -3,14 +3,15 @@ package com.example.coconut.domain.report.controller;
 
 
 import com.example.coconut.domain.report.entity.Report;
+import com.example.coconut.domain.report.form.ReportForm;
 import com.example.coconut.domain.report.service.ReportService;
+import com.example.coconut.domain.reportReply.form.ReportReplyForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,10 +35,25 @@ public class ReportController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Long id){
+    public String detail(Model model, @PathVariable("id") Long id, ReportReplyForm reportReplyForm){
         Report report = this.reportService.getReport(id);
         model.addAttribute("report", report);
         return "report/detail";
+    }
+
+    @GetMapping("/create")
+    public String reportCreate(ReportForm reportForm){
+        return "report/form";
+    }
+
+    @PostMapping("/create")
+    public String reportCreate(@Valid ReportForm reportForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "report/form";
+        }
+        this.reportService.create(reportForm.getTitle(), reportForm.getContent());
+        return "redirect:/report/list";
+
     }
 
 }
