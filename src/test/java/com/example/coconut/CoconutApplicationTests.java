ea8d0da890f16e55304600d649883ce8a797b84d
taewoo922 <kimtaewoo922@gmail.com>
2024-06-17 00:34:11 +0900

    충돌해결 4/7

--- src/test/java/com/example/coconut/CoconutApplicationTests.java
+++ src/test/java/com/example/coconut/CoconutApplicationTests.java
@@ -126,6 +126,7 @@ class CoconutApplicationTests {
 //			String thumbnailImg = "freedcs/" + "1e4bb67e-e109-4b1d-aa1a-4635d62bac15" + ".jpg";
 //			String thumbnailImg = "freedcs/" + "[사진이름]" + ".jpg";
 			String thumbnailImg = "freedcs/" + "1e4bb67e-e109-4b1d-aa1a-4635d62bac15" + ".jpg";
+			String thumbnailImg = "freedcs/" + "[사진이름]" + ".jpg";
 
 //			[사진이름]자리에 본인 폴더 안에있는 사진 이름 입력
 			User author = null;
