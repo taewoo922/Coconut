ed44f7f64247feeb57df1481e35cd39894426bb5
kimminseop99 <minseopkim99@gmail.com>
2024-06-17 16:27:51 +0900

    test merge 해결

--- src/test/java/com/example/coconut/CoconutApplicationTests.java
+++ src/test/java/com/example/coconut/CoconutApplicationTests.java
@@ -117,6 +117,7 @@ class CoconutApplicationTests {
 //	}
 
 	@Test
+
 	@DisplayName("report 데이터 저장")
 	void reportTest() {
 		for (int i = 1; i <= 300; i++) {
@@ -126,6 +127,7 @@ class CoconutApplicationTests {
 		}
 	}
 	@Test
+
 	@DisplayName("토론 데이터 저장")
 	void freeDcsTest() {
 		for (int i = 1; i <= 5; i++) {
