85c407711b48b86c74222de2030fa2e3610a0004
kimminseop99 <161572638+kimminseop99@users.noreply.github.com>
2024-06-17 16:22:55 +0900

    main page 수정 css 중복 제거 slide 이미지 수정 (#65)
    * test data 수정, list paging 오류 수정
    
    * slide Image 수정, main page 여백 제거
    
    * slide list paging css 코드 중복 제거

--- src/test/java/com/example/coconut/CoconutApplicationTests.java
+++ src/test/java/com/example/coconut/CoconutApplicationTests.java
@@ -7,6 +7,7 @@ import com.example.coconut.domain.report.service.ReportService;
 import com.example.coconut.domain.reportReply.repository.ReportReplyRepository;
 import com.example.coconut.domain.user.entity.User;
 import com.example.coconut.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,6 +39,8 @@ class CoconutApplicationTests {
 
 	@Autowired
 	private UserRepository userRepository;
+
+
 
 
 
@@ -84,33 +87,16 @@ class CoconutApplicationTests {
 
 	/*@Test
 	void contextLoads() {
+
 		for (int i = 1; i <= 300; i++) {
-			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String title = String.format("테스트 데이터입니다:[%03d]", i);
 			String content = "내용무";
-			this.reportService.create(subject, content, null, "자유토론");
+			this.reportService.create(title, content, null, "자유토론", true);
 		}
-		}*/
-//		Report r1 = new Report();
-//		r1.setTitle("질문1");
-//		r1.setContent("없어요");
-//		this.reportRepository.save(r1);  // 첫번째 질문 저장
-
-//		Report r2 = new Report();
-//		r2.setTitle("질문2");
-//		r2.setContent("있어요");
-//		this.reportRepository.save(r2);  // 첫번째 질문 저장
-
-//		Optional<Report> oq = this.reportRepository.findById(Long.valueOf(2));
-//		assertTrue(oq.isPresent());
-//		Report q = oq.get();
-
-//		ReportReply a = new ReportReply();
-//		a.setContent("네");
-//		a.setReport(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-//		this.reportReplyRepository.save(a);
-
+	}
 	@Test
-	void contextLoads() {
+	@DisplayName("토론 데이터 저장")
+	void freeDcsTest() {
 		for (int i = 1; i <= 5; i++) {
 			String subject = String.format("테스트 데이터입니다:[%03d]", i);
 			String content = "내용무";
