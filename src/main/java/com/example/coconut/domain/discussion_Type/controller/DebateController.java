e8e21e11017e557e1c57453784fcb4a48b0d1f70
taewoo922 <kimtaewoo922@gmail.com>
2024-06-21 17:30:32 +0900

    충돌해결29/29

--- src/main/java/com/example/coconut/domain/discussion_Type/controller/DebateController.java
+++ src/main/java/com/example/coconut/domain/discussion_Type/controller/DebateController.java
@@ -135,8 +135,8 @@ public class DebateController {
     @PostMapping("/debate/scrap/{id}")
     public String addScrap(@PathVariable("id") Long id) {
         scrapService.addDebateScrap(id);
-        return "redirect:/discussion/d_detail/{id}";
 
+        return "redirect:/discussion/d_detail/{id}";
     }
 
 
