010967993218eb8e6d59db7b7adfe7c6fe5288b4
kimminseop99 <minseopkim99@gmail.com>
2024-06-21 15:33:00 +0900

    rebase 7/9

--- src/main/java/com/example/coconut/domain/discussion_Type/controller/FreedcsController.java
+++ src/main/java/com/example/coconut/domain/discussion_Type/controller/FreedcsController.java
@@ -52,9 +52,9 @@ public class FreedcsController {
         freedcsList = this.freedcsService.getPostsByCategory(categoryId);
 
         Page<Freedcs> paging = this.freedcsService.getList(page, kw);
-        model.addAttribute("paging_category", paging_category);
         paging = this.freedcsService.getListByCategory(page, kw, categoryId);
 
+        model.addAttribute("paging_category", paging_category);
         model.addAttribute("paging", paging);
         model.addAttribute("kw", kw);
         model.addAttribute("freedcsList", freedcsList);
