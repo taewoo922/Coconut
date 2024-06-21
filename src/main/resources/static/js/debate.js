d14dab7fd8c52a3b2e80c2cbd9866da6d9c4775d
taewoo922 <kimtaewoo922@gmail.com>
2024-06-21 17:22:53 +0900

    충돌해결 19/20

--- src/main/resources/static/js/debate.js
+++ src/main/resources/static/js/debate.js
@@ -115,6 +115,7 @@ document.addEventListener('DOMContentLoaded', function() {
                                     }
                                 });
                             });
+                            };
                         });
 
 
